6ad75d2d1229b0bbb25992913c4c55c3d632b715
Yoshimasa Suzuki <cognac0004@gmail.com>
2021-08-30 21:24:01 +0900

    [beginning/end]_of_monthのメソッド化

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -3,13 +3,19 @@
 require 'date'
 
 today = Date.today
-beginning_of_month = Date.new(today.year, today.month, 1)
-end_of_month = Date.new(today.year, today.month + 1, 1) - 1
+
+def beginning_of_month(date)
+  Date.new(date.year, date.month, 1)
+end
+
+def end_of_month(date)
+  Date.new(date.year, date.month + 1, 1) - 1
+end
 
 puts "#{today.month}月 #{today.year}".center(20)
 puts "日 月 火 水 木 金 土"
 
-(beginning_of_month..end_of_month).each.with_index(1) do |date, index|
+(beginning_of_month(today)..end_of_month(today)).each.with_index(1) do |date, index|
   if index == 1
     print " " * (3 * date.wday)
   end
