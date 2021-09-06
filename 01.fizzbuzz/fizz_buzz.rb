91cce4686115e57087ac33cd3ee389a5e683b811
Yoshimasa Suzuki <cognac0004@gmail.com>
2021-09-06 19:45:55 +0900

    use if-elsif instead of case
    rubocopの指摘修正

--- 01.fizzbuzz/fizz_buzz.rb
+++ 01.fizzbuzz/fizz_buzz.rb
@@ -2,11 +2,14 @@
 
 (1..20).each do |num|
   output =
-    case
-    when (num % 15).zero? then "FizzBuzz"
-    when (num % 3).zero? then "Fizz"
-    when (num % 5).zero? then "Buzz"
-    else num
+    if (num % 15).zero?
+      "FizzBuzz"
+    elsif (num % 3).zero?
+      "Fizz"
+    elsif (num % 5).zero?
+      "Buzz"
+    else
+      num
     end
 
   puts output
