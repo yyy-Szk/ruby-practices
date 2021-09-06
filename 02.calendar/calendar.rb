5f17fd30c92379b1d9b08a4ac73dfd7e2939634a
Yoshimasa Suzuki <cognac0004@gmail.com>
2021-09-06 21:18:33 +0900

    rubocopの指摘修正と余計なもの削除

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -1,9 +1,9 @@
 #! /usr/bin/env ruby
+# frozen_string_literal: true
 
 require 'date'
 require 'optparse'
 
-# これモンキーパッチってやつでもっといい感じに書けるのでは？ > Dateクラス
 def beginning_of_month(date)
   Date.new(date.year, date.month, 1)
 end
@@ -11,13 +11,13 @@ end
 def end_of_month(date)
   if date.month == 12
     year = date.year + 1
-    month = 1
+    next_month = 1
   else
     year = date.year
-    month = date.month + 1
+    next_month = date.month + 1
   end
-  
-  Date.new(year, month, 1) - 1
+
+  beginning_of_month(Date.new(year, next_month)) - 1
 end
 
 today = Date.today
@@ -26,22 +26,23 @@ calendar_width = 20
 opt = OptionParser.new
 month_option = nil
 year_option = nil
-# オプションのバリデーションもするか？
-opt.on("-m MONTH") { |v| month_option = v.to_i }
-opt.on("-y YEAR") { |v| year_option = v.to_i }
+
+opt.on('-m MONTH') { |v| month_option = v.to_i }
+opt.on('-y YEAR') { |v| year_option = v.to_i }
 opt.parse!(ARGV)
 
-target_date = Date.new(year_option || today.year, month_option || today.month, 1)
+target_date = Date.new(year_option || today.year, month_option || today.month)
 date_range = beginning_of_month(target_date)..end_of_month(target_date)
 
+# 出力担当
 puts "#{target_date.month}月 #{target_date.year}".center(calendar_width)
-puts "日 月 火 水 木 金 土"
+puts '日 月 火 水 木 金 土'
 
 date_range.each.with_index(1) do |date, index|
-  if index == 1
-    print " " * (3 * date.wday)
-  end
-  print date.day.to_s.rjust(2) + " "
+  print ' ' * (3 * date.wday) if index == 1
+
+  print date.day.to_s.rjust(2)
+  print ' ' # 日付間の空白
+
   puts if date.saturday?
 end
-
