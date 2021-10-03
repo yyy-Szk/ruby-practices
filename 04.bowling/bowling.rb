475dfcf55f3f3869fc0531562cd27bfc7b053dcb
Yoshimasa Suzuki <cognac0004@gmail.com>
2021-10-04 01:18:02 +0900

    strike含め、とりあえず動作するように修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,30 +1,64 @@
 #! /usr/bin/env ruby
 # frozen_string_literal: true
 
+def split_flame(data)
+  data.split(',').each_with_object([]) do |v, array|
+    case v
+    # ストライクの場合,一投目のスコアを10、二投目のスコアを0とする
+    when 'X' then array.push(10, 0)
+    else array.push(v.to_i)
+    end
+  end.each_slice(2).to_a
+end
+
 # 結果は第一引数に渡される
-result = ARGV[0]
+game_data = ARGV[0]
 
 # 入力忘れ用
-if result.nil? 
+if game_data.nil? 
   puts "第一引数に結果を渡してください。"
   return
  end
 
+p split_flame(game_data)
 score = 0
+is_strike = false
+is_second_strike = false
 is_spare = false
-result.split(',').each_slice(2).with_index(1) do |data, index|
-  flame_score = data.sum(&:to_i)
+split_flame(game_data).each.with_index(1) do |data, index|
+  first_throw, second_throw = data
+  flame_score = data.sum
 
-  # 前回の記録がspareの場合、一投目の点数を加算
-  # (ラストフレームを除く > ラストは投球数が増える)
-  score += data[0].to_i if is_spare && index != 10
+  # 二連続ストライクのみ、ラストフレーム以降に加算を行う可能性があるため外に出す
+  score += first_throw if is_second_strike
+  # ラストフレーム以降は、投球数が増えるだけで特別な処理はいらない = 9 投目の時点で全てのフラグをfalseにする
+  unless index > 10
+  # 前回の記録がspare/strikeかつ今回もstrikeの場合、一投目の点数を加算 / strikeの場合、フレーム全体の点数を加算
+    score += first_throw if is_spare
+    score += flame_score if is_strike
+    # なんかメソッドにしたい
+    if first_throw == 10
+      is_second_strike = is_strike ? true : false
+      is_strike = true
+      is_spare = false
+    elsif flame_score == 10
+      is_spare = true
+      is_second_strike = false
+      is_strike = false
+     else
+      is_spare = false
+      is_second_strike = false
+      is_strike = false
+    end
+  else
+    is_spare = false
+    is_second_strike = false
+    is_strike = false
+  end
+
   score += flame_score
-
-  p '=========='
-  p score
-  p '=========='
-
-  is_spare = flame_score == 10 ? true : false
+  # binding.irb
+  # p 'score', score
 end
 
 puts score
