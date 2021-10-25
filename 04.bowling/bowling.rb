d48a0cc4c7c6aeb400c088627f8ec8ca65f81774
Yoshimasa Suzuki <cognac0004@gmail.com>
2021-10-26 08:49:25 +0900

    処理をメソッドに分けて綺麗にする

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -14,60 +14,61 @@ class BowlingScoreCalculator
     @total_score = 0
     is_spare, is_strike, is_double_strike = false, false, false
 
-    split_by_frame.each.with_index(1) do |data, frame_count|
-      is_last_frame = frame_count > 10
+    game_data_splitted_by_frame.each.with_index(1) do |data, frame_count|
+      # 通常は1フレームにつき2投
       first_throw, second_throw = data
-      frame_score = data.sum # 最終フレームは投球数が変動するので、その考慮で sum にて算出
-
+      frame_score = data.sum 
       bonus_score = calculate_bonus_score(
         first_throw: first_throw,
         second_throw: second_throw,
         is_spare: is_spare,
         is_strike: is_strike,
-        is_double_strike: is_double_strike,
-        is_last_frame: is_last_frame
+        is_double_strike: is_double_strike
       )
-
-      if first_throw == 10
-        is_double_strike = is_strike ? true : false
-        is_strike = true
-        is_spare = false
-      elsif frame_score == 10
-        is_spare = true
-        is_double_strike = false
-        is_strike = false
-      else
-        is_spare = false
-        is_double_strike = false
-        is_strike = false
-      end
-
-      if is_last_frame
-        is_spare = false
-        is_double_strike = false
-        is_strike = false
-      end
+      is_spare, is_strike, is_double_strike = check_bonus_flags(first_throw: first_throw, second_throw: second_throw, is_strike: is_strike, frame_count: frame_count)
 
       @total_score += (frame_score + bonus_score)
     end
   end
 
  private
+  def check_bonus_flags(first_throw:, second_throw:, is_strike:, frame_count:)
+    # 最終フレーム直前の場合処理が変わる
+    if frame_count > 9
+      is_double_strike = (first_throw == 10 && is_strike) ? true : false
+      is_spare = false
+      is_strike = false
 
-  def calculate_bonus_score(first_throw:, second_throw:, is_strike:, is_double_strike:, is_spare:, is_last_frame:)
-    score = 0
-    # ラストフレームは、基本的に投球数が増えるだけ。
-    # ただし、二連続ストライクのみラストフレームに加算を行う可能性がある。
-    unless is_last_frame
-      score += (first_throw + second_throw) if is_strike
-      score += first_throw if is_spare
+      return [is_spare, is_strike, is_double_strike]
     end
+
+    if first_throw == 10
+      is_double_strike = is_strike ? true : false
+      is_strike = true
+      is_spare = false
+    elsif (first_throw + second_throw) == 10
+      is_spare = true
+      is_double_strike = false
+      is_strike = false
+    else
+      is_spare = false
+      is_double_strike = false
+      is_strike = false
+    end
+
+    [is_spare, is_strike, is_double_strike]
+  end
+
+  def calculate_bonus_score(first_throw:, second_throw:, is_strike:, is_double_strike:, is_spare:)
+    score = 0
+    score += (first_throw + second_throw) if is_strike
+    score += first_throw if is_spare
     score += first_throw if is_double_strike
 
     score
   end
 
-  def split_by_frame
+  def game_data_splitted_by_frame
     strike = "X"
     game_data.split(',').each_with_object([]) do |v, array|
       case v
@@ -77,7 +78,6 @@ class BowlingScoreCalculator
     end.each_slice(2).to_a
   end
 end
-
 
 # 結果は第一引数に渡される
 game_data = ARGV[0]
