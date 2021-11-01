303c59458d762a260ab3a56835223d0d32a48276
Yoshimasa Suzuki <cognac0004@gmail.com>
2021-11-01 22:07:20 +0900

    lsコマンドの土台完成（デザインは無視）

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -2,17 +2,26 @@
 # frozen_string_literal: true
 
 class ListSegments
-  def initialize
+  attr_reader :path
+
+  def initialize(path)
+    @path = path
   end
 
-  def self.call
-    self.new().call
+  def self.call(path = '.')
+    new(path).call
   end
 
   def call
-    puts 'called'
+    Dir.foreach(path) do |file_name|
+      # optionは未実装なので、隠しファイルは表示しない
+
+      next if file_name.start_with?('.')
+
+      print file_name, (' ' * 6)
+    end
   end
 end
 
-ListSegments.call
-
+path = ARGV[0] || '.'
+ListSegments.call(path)
