2bc7869e3e66ee0dbdbf024b380aca94677801af
Yoshimasa Suzuki <cognac0004@gmail.com>
2021-12-13 21:03:09 +0900

    rubocopによる指摘を修正、不要な引数削除

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -17,10 +17,9 @@ class BowlingScoreCalculator
   def calculate
     # total_score は 計算するまで nil にしたいのでここで入れる
     @total_score = 0
-    bonus_type = nil
 
     frames.each.with_index(0) do |frame_scores, index|
-      bonus_score = calculate_bonus_score(frame_scores, bonus_type, index)
+      bonus_score = calculate_bonus_score(frame_scores, index)
       @total_score += (frame_scores.sum + bonus_score)
     end
   end
@@ -35,27 +34,27 @@ class BowlingScoreCalculator
     frames[index - 1]
   end
 
-  def calculate_bonus_score(frame_scores, bonus_type, index)
+  def calculate_bonus_score(frame_scores, index)
     # 最初のフレームは、ボーナススコアが0
     return 0 if index < 1
 
+    # ストライク
     if before_frame_scores(index).first == 10
-      if frame_scores.first == 10 && index >= 9
-        # 最終フレームでの、ダブルストライクの場合
-        # => 最後のフレームは、ストライクの場合投球数が増える関係で、ストライクの時のボーナススコアの計算方法が異なる
-        frame_scores.first + frame_scores.second.to_i
-      elsif frame_scores.first == 10
-        # 2連続ストライクの場合
-        frame_scores.first + next_frame_scores(index).first
-      else
-        # 前回がストライクの場合（フレーム計算時に0をdeleteしているので、2投目以降がnilの可能性がある > to_iを入れて対策）
-        frame_scores.first + frame_scores.second.to_i
-      end
-    elsif before_frame_scores(index).sum == 10
-      frame_scores.first
-    else
-      0
+      # 最終フレームでの、ダブルストライクの場合
+      # => 最後のフレームは、ストライクの場合投球数が増える関係で、ストライクの時のボーナススコアの計算方法が異なる
+      return frame_scores.first + frame_scores.second.to_i if frame_scores.first == 10 && index >= 9
+
+      # 2連続ストライクの場合
+      return frame_scores.first + next_frame_scores(index).first if frame_scores.first == 10
+
+      # 前回がストライクの場合（フレーム計算時に0をdeleteしているので、2投目以降がnilの可能性がある > to_iを入れて対策）
+      return frame_scores.first + frame_scores.second.to_i
     end
+
+    # スペア
+    return frame_scores.first if before_frame_scores(index).sum == 10
+
+    0
   end
 
   def check_next_frame_bonus_type(frame_scores, bonus_type)
@@ -82,16 +81,16 @@ class BowlingScoreCalculator
     end.each_slice(2).to_a
 
     # 最終フレームを最適化する（フレーム数が10になるように調整）
-    throws_by_splitted.each_with_object([]) do |_throw, array|
+    throws_by_splitted.each_with_object([]) do |frame_throws, array|
       if array.size < 10
         # 最終フレームの一投目がストライクの場合を考慮
-        _throw.delete(0) if array.size >= 9
-        array << _throw
+        frame_throws.delete(0) if array.size >= 9
+        array << frame_throws
         next
       end
 
-      _throw.delete(0)
-      array.last.concat(_throw)
+      frame_throws.delete(0)
+      array.last.concat(frame_throws)
     end
   end
 end
