10f62c62165fac3878d82cac7d8da26b99dd43fb
Yoshimasa Suzuki <cognac0004@gmail.com>
2022-01-06 21:32:46 +0900

    rspecの書き方を適切な形へ修正

--- spec/ls_spec.rb
+++ spec/ls_spec.rb
@@ -5,23 +5,23 @@ require_relative '../05.ls/ls'
 RSpec.describe LS do
   describe 'self.call' do
     # ruby-practicesディレクトリから実行されることを想定
-    example '動作の検証' do
-      # 引数なしの場合
-      expect { described_class.call }.to(output(
-        <<~FILE_LIST
+    context "引数なしの場合" do
+      example 'コマンドを実行したディレクトリ内のファイル一覧 が表示される' do
+        expect { LS.call }.to(output(<<~FILE_LIST).to_stdout)
           01.fizzbuzz   05.ls               09.wc_object
           02.calendar   06.wc               README.md
           03.rake       07.bowling_object   spec
           04.bowling    08.ls_object
         FILE_LIST
-      ).to_stdout)
+      end
+    end
 
-      # 引数ありの場合
-      expect { described_class.call('05.ls') }.to(output(
-        <<~FILE_LIST
+    context "引数ありの場合" do
+      example '引数として指定したディレクトリ内のファイル一覧 が表示される' do
+        expect { LS.call('05.ls') }.to(output(<<~FILE_LIST).to_stdout)
           ls.rb
         FILE_LIST
-      ).to_stdout)
+      end
     end
   end
 end
